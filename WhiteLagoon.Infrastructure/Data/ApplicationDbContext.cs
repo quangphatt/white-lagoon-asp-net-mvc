94f0c5eea6d3e9264983e29ebcf473dfa84ce093
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2024-07-04 14:24:46 +0700

    create ApplicationDbContext

---
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WhiteLagoon.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        }
+    }
+}
