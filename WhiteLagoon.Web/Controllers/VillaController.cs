80c777d5ec991dd38d0c37a4da14ac8b1b394064
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2024-07-05 15:41:24 +0700

    Villa CRUD

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public VillaController(ApplicationDbContext dbContext)
+        { 
+            _dbContext = dbContext;
+        }    
+
+        public IActionResult Index()
+        {
+            var villas = _dbContext.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Villas.Add(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _dbContext.Villas.FirstOrDefault(u=>u.Id==villaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+
+                _dbContext.Villas.Update(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _dbContext.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _dbContext.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+                _dbContext.Villas.Remove(objFromDb);
+                _dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+    }
+}
