8ae9f9f132c98418c27968406d633c7a3bad17c8
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2024-07-19 00:13:47 +0700

    Amenity

---
+++ WhiteLagoon.Infrastructure/Repository/AmenityRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class AmenityRepository : Repository<Amenity>, IAmenityRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AmenityRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Amenity entity)
+        {
+            _db.Amenities.Update(entity);
+        }
+    }
+}
--- WhiteLagoon.Infrastructure/Repository/UnitOfWork.cs
+++ WhiteLagoon.Infrastructure/Repository/UnitOfWork.cs
@@ -13,12 +13,14 @@ namespace WhiteLagoon.Infrastructure.Repository
         private readonly ApplicationDbContext _db;
         public IVillaRepository Villa { get; private set; }
         public IVillaNumberRepository VillaNumber { get; private set; }
+        public IAmenityRepository Amenity { get; private set; }
 
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Villa = new VillaRepository(_db);
             VillaNumber = new VillaNumberRepository(_db);
+            Amenity = new AmenityRepository(_db);
         }
 
         public void Save()
