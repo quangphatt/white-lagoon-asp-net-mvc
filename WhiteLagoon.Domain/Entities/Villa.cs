8686eaea028a3ca72ca1f52672b35019488b5db2
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2024-07-22 11:44:00 +0700

    Villa Availability

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -30,5 +30,7 @@ namespace WhiteLagoon.Domain.Entities
         public DateTime? UpdatedDate { get; set; }
         [ValidateNever]
         public IEnumerable<Amenity> VillaAmenity { get; set; }
+        [NotMapped]
+        public bool IsAvailable { get; set; } = true;
     }
 }
--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -24,6 +24,33 @@ public class HomeController : Controller
         return View(homeVM);
     }
 
+    [HttpPost]
+    public IActionResult Index(HomeVM homeVM)
+    {
+        homeVM.VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity");
+        return View(homeVM);
+    }
+
+    public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
+    {
+        var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
+        foreach (var villa in villaList)
+        {
+            if (villa.Id % 2 == 0)
+            {
+                villa.IsAvailable = false;
+            }
+        }
+        HomeVM homeVM = new()
+        {
+            CheckInDate = checkInDate,
+            VillaList = villaList,
+            Nights = nights
+        };
+
+        return PartialView("_VillaList", homeVM);
+    }
+
     public IActionResult Privacy()
     {
         return View();
