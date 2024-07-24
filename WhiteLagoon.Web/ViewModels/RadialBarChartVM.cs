d9f7b9248ba0919618dd450f0c2c49f6e1653058
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2024-07-24 17:15:13 +0700

    add Chart

---
+++ WhiteLagoon.Web/ViewModels/RadialBarChartVM.cs
@@ -0,0 +1,10 @@
+﻿namespace WhiteLagoon.Web.ViewModels
+{
+    public class RadialBarChartVM
+    {
+        public decimal TotalCount { get; set; }
+        public decimal CountInCurrentMonth { get; set; }
+        public bool HasRatioIncreased { get; set; }
+        public int[] Series { get; set; }
+    }
+}
